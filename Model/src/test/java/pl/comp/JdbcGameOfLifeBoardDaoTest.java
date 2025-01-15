1fd3573230db2aeb69eb12d4752127f541bf75f6
251670 Żelaziński Bartosz <251670@edu.p.lodz.pl>
2025-01-15 20:17:05 +0100

    Przetestowanie możliwych wyjątków

--- Model/src/test/java/pl/comp/JdbcGameOfLifeBoardDaoTest.java
+++ Model/src/test/java/pl/comp/JdbcGameOfLifeBoardDaoTest.java
@@ -2,10 +2,11 @@ package pl.comp;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pl.comp.exceptions.DaoReadException;
+import pl.comp.exceptions.UnknownGameException;
 import pl.comp.storage.Dao;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 import static pl.comp.storage.GameOfLifeBoardDaoFactory.createDatabaseGameOfLifeBoardDao;
 
 public class JdbcGameOfLifeBoardDaoTest {
@@ -31,8 +32,23 @@ public class JdbcGameOfLifeBoardDaoTest {
                     assertEquals(board.get(i, j), readBoard.get(i, j));
                 }
             }
+
+            assertNotSame(board, readBoard);
         } catch (Exception e) {
             fail(e.getMessage());
         }
+    }
+
+    @Test
+    public void testIncorrectGameName() {
+        try (Dao<GameOfLifeBoard> dao = createDatabaseGameOfLifeBoardDao("zbytdluganazwadlagrypowyzej15znakow", true)) {
+            assertThrows(DaoReadException.class, dao::read);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        assertThrows(UnknownGameException.class, () -> {
+            Dao<GameOfLifeBoard> dao = createDatabaseGameOfLifeBoardDao("", true);
+            dao.close();
+        });
     }
 }
